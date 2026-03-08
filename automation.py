726504df8097bd22e66239e93e886bfc615b9fe7
Victor Augusto <victorcosta939@gmail.com>
2026-03-08 20:35:11 -0300

    setting up for the rest of the challenge

--- automation.py
+++ automation.py
@@ -1,7 +1,9 @@
 import os
 
-for i in range(11,31):
+for i in range(30,11,-1):
+    ignore_pattern = f"day{i}-/*"
     directory = f"./day{i}-"
+    print(ignore_pattern)
     os.mkdir(directory)
     open(f"{directory}/main_cpp.cpp", "w").close()
     open(f"{directory}/main_py.py", "w").close()
