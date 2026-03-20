abb6b662603744a2d360e0c63e94f90379d089d4
Victor Augusto <victorcosta939@gmail.com>
2026-03-20 20:52:21 -0300

    feat: day23 more bst but with bfs

---
+++ day23-bst_2/main_py.py
@@ -0,0 +1,39 @@
+import queue
+
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+    def levelOrder(self,root):
+        q = queue.Queue()
+        q.put(root)
+        while not q.empty():
+            item = q.get()
+            if item.left:
+                q.put(item.left)
+            if item.right:
+                q.put(item.right)
+            print(item.data,end=' ')
+        print()
+            
+
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+myTree.levelOrder(root)
